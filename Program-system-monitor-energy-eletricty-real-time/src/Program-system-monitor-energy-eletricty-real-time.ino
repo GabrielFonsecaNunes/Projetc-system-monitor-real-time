e04829c78234c2099f673116a5a2631b86ede1be
GabrielFonsecaNunes <fonseca.nunes1996@gmail.com>
2021-02-10 04:58:35 -0300

    Add lib Pzem-004T-v30

--- Program-system-monitor-energy-eletricty-real-time/src/Program-system-monitor-energy-eletricty-real-time.ino
+++ Program-system-monitor-energy-eletricty-real-time/src/Program-system-monitor-energy-eletricty-real-time.ino
@@ -1,9 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
 
+/* 
+ *  Placa de desenvolvimento ESP32 com PZEM-T04
+ *  Projeto sistema monitoramento do gasto de energia eletrica em tempo real
+*/
+
+#include <PZEM004Tv30.h>
+
+void setup() {
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
